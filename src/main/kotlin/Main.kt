72564cd38e94a89a55962d5b9445b354d95948df
Ricardo <rgallego@iesrafaelalberti.es>
2022-01-23 17:24:45 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,22 +9,40 @@ import java.util.logging.LogManager
 
 val l = LogManager.getLogManager().getLogger("").apply { level = Level.OFF }
 
+/**
+ * Esta es nuestra clase, a la que le introduciremos de input la ruta del archivo.
+ * Ruta es el input de nuestra clase.
+ */
 class CatalogoLibrosXML(ruta: String) {
 
     val xmlDoc = readXml(ruta)
 
+    /**
+     * Inicializamos el constructor comprobando que la ruta existe. Lo inicializamos ahora por jerarquía normativa.
+     */
     init {constructor(ruta)}
 
     //No estoy seguro a que te refieres con abortar. Lo voy a interpretar como que se para el programa si el path introducido está mal
+    /**
+     * Aqui nuestro método constructor
+     */
     private fun constructor(cargador: String) = try { readXml(cargador) } catch (e: java.io.FileNotFoundException) { System.exit(-1) }
 
+    /**
+     * El método existeLibro determinará si el libro introducido existe y devuelve @return [Boolean] según el resultado.
+     */
     fun existeLibro(id: String): Boolean {
         val bookList: NodeList = xmlDoc.getElementsByTagName("book")
         //La siguiente variable estará por defecto en false y se cambiará a true si se encuentra la id en una posterior línea. El return del método será esta variable
         var existeBoolean: Boolean = false
+        /**
+         * En este método se creará una lista de nodos a buscar según el tag introducido, en este caso será "book".
+         */
         for (i in 0 until bookList.length) {
             var bookNode: Node = bookList.item(i)
-
+            /**
+             * Después se buscará si el nodo es igual al que se está buscando y si es así se comprobará que contiene la id introducida incial y finalmente devuelve el boolean de variable @return [existeBoolean]
+             */
             if (bookNode.getNodeType() === Node.ELEMENT_NODE) {
                 val elem = bookNode as Element
                 val mMap = mutableMapOf<String, String>()
@@ -40,6 +58,9 @@ class CatalogoLibrosXML(ruta: String) {
         return existeBoolean
     }
 
+    /**
+     * El método infoLibro devolverá un mapa con los tags y respectiva información del libro a introducir con @param idLibro.
+     */
     fun infoLibro(idLibro: String): Map<String,Any> {
 
         val map = mutableMapOf<String,Any>()
@@ -50,8 +71,14 @@ class CatalogoLibrosXML(ruta: String) {
             val elem = lista[j] as Element
             val mMap = obtenerAtributosEnMapKV(elem)
 
+            /**
+             * Con el mapa creado se buscará si el mapa en las distintas posiciones contiene el valor introducido con idLibro.
+             */
             for (i in 0 until mMap.size)
             {
+                /**
+                 * De ser así, se rellenará el mapa con la información correspondiente y se devolverá.
+                 */
                 if (mMap.containsValue(idLibro)) {
                     map["id"] = idLibro
                     map["Author"] = elem.getElementsByTagName("author").item(i).textContent
